c456779342ba2cabc76961fe37469cb51080dabc
Josje <paul.verbael@gmail.com>
2025-07-13 19:08:59 +0200

    Added Executed Scheduler in Main

--- src/main/java/nl/radiantrealm/library/Main.java
+++ src/main/java/nl/radiantrealm/library/Main.java
@@ -1,6 +1,16 @@
 package nl.radiantrealm.library;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
     public static void main(String[] args) {
+    }
+
+    public static void scheduleAtFixedRate(long initialDelay, long period, Runnable task) {
+        executorService.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.MILLISECONDS);
     }
 }
