6432cee341434c59bead65ab2985d35fc1504c58
Josje <paul.verbael@gmail.com>
2025-07-17 16:31:29 +0200

    Added helper method to API Request
    Regarding issue https://github.com/radiantrealm-nl/radiantrealm-library/issues/1, added getBody method to get either the Json object contained within the request body or null.

--- src/main/java/nl/radiantrealm/library/server/Request.java
+++ src/main/java/nl/radiantrealm/library/server/Request.java
@@ -4,4 +4,9 @@ import com.google.gson.JsonObject;
 
 import java.util.Optional;
 
-public record Request(String method, Optional<JsonObject> body) {}
+public record Request(String method, Optional<JsonObject> body) {
+
+    public JsonObject getBody() {
+        return body.orElse(null);
+    }
+}
