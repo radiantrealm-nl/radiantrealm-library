dae65ea76d693f7aa1f3387d14a193709833282f
Josje <paul.verbael@gmail.com>
2025-07-17 23:17:50 +0200

    Added utility methods to Result<T>

--- src/main/java/nl/radiantrealm/library/utils/Result.java
+++ src/main/java/nl/radiantrealm/library/utils/Result.java
@@ -4,7 +4,13 @@ import java.util.Optional;
 
 public record Result<T>(Optional<T> object, Optional<Throwable> throwable) {
 
+    public static <T> Result<T> ok(T object) {
+        return new Result<>(Optional.of(object), Optional.empty());
+    }
 
+    public static <T> Result<T> error(Throwable throwable) {
+        return new Result<>(Optional.empty(), Optional.of(throwable));
+    }
 
     public T getObject() {
         return object.orElse(null);
@@ -18,4 +24,9 @@ public record Result<T>(Optional<T> object, Optional<Throwable> throwable) {
         return object.isEmpty();
     }
 
+    public void throwIt() throws Throwable {
+        if (throwable.isPresent()) {
+            throw throwable.get();
+        }
+    }
 }
