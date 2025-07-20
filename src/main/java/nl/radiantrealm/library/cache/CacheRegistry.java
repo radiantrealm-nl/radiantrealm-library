fb95497a6fb82f667a017a079c222ceb4d45960b
Josje <paul.verbael@gmail.com>
2025-07-21 01:20:10 +0200

    Refactor CacheRegistry and add Cache controller
    Refactored CacheRegistry to implement ApplicationService, improved cache cleanup and lifecycle management, and simplified cache data structures. Added a new Cache controller class with a static CollieCache instance for centralized cache access.

--- src/main/java/nl/radiantrealm/library/cache/CacheRegistry.java
+++ src/main/java/nl/radiantrealm/library/cache/CacheRegistry.java
@@ -1,134 +1,90 @@
 package nl.radiantrealm.library.cache;
 
+import nl.radiantrealm.library.ApplicationService;
+import nl.radiantrealm.library.ApplicationStatus;
 import nl.radiantrealm.library.Main;
 import nl.radiantrealm.library.utils.Result;
 
-import java.util.*;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
 
-public abstract class CacheRegistry<K, V> {
-    private final Map<K, V> dataMap = new ConcurrentHashMap<>();
-    private final Map<K, Long> expiryMap = new ConcurrentHashMap<>();
+public abstract class CacheRegistry<K, V> implements ApplicationService {
+    private final Map<K, V> data = new ConcurrentHashMap<>();
+    private final Map<K, Long> expiry = new ConcurrentHashMap<>();
 
-    private final int expiry;
+    private ScheduledFuture<?> scheduledFuture;
+    private final int duration;
 
-    /**
-     * Constructor for the {@link CacheRegistry} class.
-     * Initializes registry wiith a specified time and schdules a periodic cleanup task.
-     *
-     * @param expiry The expiry time in milliseconds for cached entries.
-     * */
-    public CacheRegistry(int expiry) {
-        this.expiry = expiry;
-
-        Main.scheduleAtFixedRate(0, 60000, () -> {
-            long current = System.currentTimeMillis();
-
-            for (Map.Entry<K, Long> entry : expiryMap.entrySet()) {
-                if (current > entry.getValue()) {
-                    dataMap.remove(entry.getKey());
-                    expiryMap.remove(entry.getKey());
-                }
-            }
-        });
+    public CacheRegistry(int duration) {
+        this.duration = duration;
     }
 
-    /**
-     * Loads a value associated with a given key. Subclasses must implement this method to define how the value for the specified key is retrieved or generated.
-     *
-     * @param key The key for which teh value needs to be loaded. Must not be null.
-     * @return The value associated with the given key, or null if no vlaue is found.
-     * @throws Exception If an error occurs during the loading process.
-     * */
-    protected abstract Result<V> load(K key) throws Exception;
-
-    /**
-     * Loads multiple values associated with their respective keys. Subclasses must implement this method to define how the values for the specified keys are retrieved or generated.
-     *
-     * @param keys A list of keys for which the values need to be loaded. Must not be null, empty, or contain only one key.
-     * @return A map containing the keys and their associated values. Missing keys may be excluded from the map.
-     * @throws Exception If an error occurs during the loading process.
-     * @since 1.0.0
-     * */
-    protected abstract Map<K, Result<V>> load(List<K> keys) throws Exception;
-
-    private void put(K key, V value) {
-        dataMap.put(key, value);
-        expiryMap.put(key, System.currentTimeMillis() + expiry);
-    }
-
-    /**
-     * Retrieves an optional value associated with the given key from the cache.
-     * If the key is not present in the cache, the value is loaded using the {@link #load(Object)} method.
-     * If the value is successfully loaded, it is stored in the cache with an expiry time.
-     *
-     * @param key The key for which the value needs to be retrieved. Must not be null.
-     * @return An {@link Result} containing the value if found or successfully loaded, or an empty {@link Result} if the key is null or the value could not be loaded.
-     * @since 1.0.0
-     * */
-    public Result<V> get(K key) {
-        if (key == null) return Result.error(new IllegalArgumentException("Invalid key."));
-
-        if (dataMap.containsKey(key)) {
-            return Result.ok(dataMap.get(key));
-        }
-
+    @Override
+    public void start() throws Exception {
         try {
-            Result<V> value = load(key);
-
-            if (value.isObjectEmpty()) {
-                return value;
-            }
-
-            put(key, value.getObject());
-            return value;
+            scheduledFuture = Main.scheduleAtFixedRate(0, 60000, this::cleanUpCache);
         } catch (Exception e) {
-            return Result.error(e);
+            throw new Exception("Failed to setup cache registry.", e);
         }
     }
 
-    /**
-     * Retrieves a key-value map based on the provided list of keys.
-     * For each key in the list, the method attempts to retrieve the associated value from the cache.
-     * If a key is not found in the cache, it is added to a list of keys to be loaded on demand.
-     * The method then loads values for the missing keys using the {@link #load(List)} method and combines the results.
-     *
-     * @param keys A list of keys for which values need to be retrieved. Must not be null or empty.
-     * @return A map containing the keys and their associated values. If a key is not found or cannot be loaded, it will not appear in the map.
-     * @since 1.0.0
-     *
-     * @deprecated since v1.1.3 needs rework on the Result<V> record.
-     * */
-    @Deprecated
-    public Map<K, Optional<V>> get(List<K> keys) {
-        if (keys == null || keys.isEmpty()) return Map.of();
+    @Override
+    public void stop() throws Exception {
+        try {
+            scheduledFuture.cancel(true);
 
-        Map<K, Optional<V>> result = new HashMap<>(keys.size());
-        List<K> missingKeys = new ArrayList<>(keys.size());
+            data.clear();
+            expiry.clear();
+        } catch (Exception e) {
+            throw new Exception("Failed to stop cache registry.");
+        }
+    }
 
-        for (K key : keys) {
-            if (dataMap.containsKey(key)) {
-                result.put(key, Optional.of(dataMap.get(key)));
-            } else {
-                missingKeys.add(key);
+    @Override
+    public ApplicationStatus status() throws Exception {
+        return ApplicationStatus.RUNNING;
+    }
+
+    protected abstract Result<V> load(K key);
+
+    private void cleanUpCache() {
+        long timestamp = System.currentTimeMillis();
+
+        for (Map.Entry<K, Long> entry : expiry.entrySet()) {
+            if (timestamp > entry.getValue()) {
+                remove(entry.getKey());
             }
         }
+    }
 
-//        try {
-//            Map<K, Optional<V>> loadedMap = load(missingKeys);
-//
-//            for (Map.Entry<K, Optional<V>> entry : loadedMap.entrySet()) {
-//                if (entry.getValue().isEmpty()) {
-//                    result.put(entry.getKey(), Optional.empty());
-//                } else {
-//                    result.put(entry.getKey(), entry.getValue());
-//                    put(entry.getKey(), entry.getValue().get());
-//                }
-//            }
-//        } catch (Exception e) {
-//            logger.error("Failed to load missing keys.", e);
-//        }
+    public Result<V> get(K key) {
+        if (key == null) {
+            return Result.error(new IllegalArgumentException("Key can not be null."));
+        }
+
+        if (data.containsKey(key)) {
+            return Result.ok(data.get(key));
+        }
+
+        Result<V> result = load(key);
+
+        if (!result.isObjectEmpty()) {
+            put(key, result.getObject());
+        }
 
         return result;
+    }
+
+    public void put(K key, V value) {
+        if (key == null || value == null) return;
+
+        data.put(key, value);
+        expiry.put(key, System.currentTimeMillis() + duration);
+    }
+
+    public void remove(K key) {
+        data.remove(key);
+        expiry.remove(key);
     }
 }
---
+++ src/main/java/nl/radiantrealm/library/controller/Cache.java
@@ -0,0 +1,9 @@
+package nl.radiantrealm.library.controller;
+
+import nl.radiantrealm.library.cache.CollieCache;
+
+public class Cache {
+    private static final CollieCache collieCache = new CollieCache();
+
+    private Cache() {}
+}
