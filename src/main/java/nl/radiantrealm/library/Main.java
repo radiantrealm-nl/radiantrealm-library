3e08b28ae4fe382f7fc7bbe0d4c065e7fab2f569
Josje <paul.verbael@gmail.com>
2025-07-21 01:20:04 +0200

    Updated ExecutorService to return ScheduledFuture

--- src/main/java/nl/radiantrealm/library/Main.java
+++ src/main/java/nl/radiantrealm/library/Main.java
@@ -2,6 +2,7 @@ package nl.radiantrealm.library;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class Main extends ApplicationController {
@@ -19,7 +20,7 @@ public class Main extends ApplicationController {
     public static void main(String[] args) {
     }
 
-    public static void scheduleAtFixedRate(long initialDelay, long period, Runnable task) {
-        executorService.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.MILLISECONDS);
+    public static ScheduledFuture<?> scheduleAtFixedRate(long initialDelay, long period, Runnable task) {
+        return executorService.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.MILLISECONDS);
     }
 }
