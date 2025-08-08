13b7f1e2f609824c16d5ada11d2c72d16290fa21
Josje <paul.verbael@gmail.com>
2025-08-08 23:46:20 +0100

    MimeType integration for WsopCode

--- src/main/java/nl/radiantrealm/library/enumerator/MimeType.java
+++ src/main/java/nl/radiantrealm/library/enumerator/MimeType.java
@@ -1,36 +1,52 @@
 package nl.radiantrealm.library.enumerator;
 
 public enum MimeType {
-    TEXT("text/plain"),
-    HTML("text/html"),
-    CSS("text/css"),
-    JS("text/js"),
+    TEXT("text/plain", 0x01),
+    HTML("text/html", 0x02),
+    CSS("text/css", 0x03),
+    JS("text/js", 0x04),
 
-    JSON("application/json"),
-    XML("application/xml"),
-    PDF("application/pdf"),
-    ZIP("application/zip"),
-    BINARY("application/octet-stream"),
+    JSON("application/json", 0x05),
+    XML("application/xml", 0x06),
+    PDF("application/pdf", 0x07),
+    ZIP("application/zip", 0x08),
+    BINARY("application/octet-stream", 0x09),
 
-    JPEG("image/jpeg"),
-    PNG("image/png"),
-    GIF("image/gif"),
-    SVG("image/svg+xml"),
+    JPEG("image/jpeg", 0x0A),
+    PNG("image/png", 0x0B),
+    GIF("image/gif", 0x0C),
+    SVG("image/svg+xml", 0x0D),
 
-    MP3("audio/mpeg"),
-    OGG("audio/ogg"),
-    WAV("audio/wav"),
+    MP3("audio/mpeg", 0x0E),
+    OGG("audio/ogg", 0x0F),
+    WAV("audio/wav", 0x10),
 
-    MP4("video/mp4"),
-    WEBM("video/webm");
+    MP4("video/mp4", 0x11),
+    WEBM("video/webm", 0x12);
 
     private final String type;
+    private final int hex;
 
-    MimeType(String type) {
+    MimeType(String type, int hex) {
         this.type = type;
+        this.hex = hex;
     }
 
     public String getType() {
         return type;
+    }
+
+    public int getHex() {
+        return hex;
+    }
+
+    public static MimeType getHex(int hex) {
+        for (MimeType mimeType : MimeType.values()) {
+            if (mimeType.hex == hex) {
+                return mimeType;
+            }
+        }
+
+        return null;
     }
 }
--- src/main/java/nl/radiantrealm/library/enumerator/WsopCode.java
+++ src/main/java/nl/radiantrealm/library/enumerator/WsopCode.java
@@ -1,21 +1,28 @@
 package nl.radiantrealm.library.enumerator;
 
 public enum WsopCode {
-    CONTINUATION(0x0),
-    TEXT_UTF8(0x1),
-    BINARY(0x2),
-    CLOSE(0x8),
-    PING(0x9),
-    PONG(0xA);
+    CONTINUATION(0x0, true),
+    TEXT_UTF8(0x1, false),
+    BINARY(0x2, false),
+    MIME_TYPE(0x3, false),
+    CLOSE(0x8, true),
+    PING(0x9, true),
+    PONG(0xA, true);
 
     private final int code;
+    private final boolean hasBody;
 
-    WsopCode(int code) {
+    WsopCode(int code, boolean hasBody) {
         this.code = code;
+        this.hasBody = hasBody;
     }
 
     public int getCode() {
         return code;
+    }
+
+    public boolean hasBody() {
+        return hasBody;
     }
 
     public static WsopCode getWsop(int integer) {
