e4211e428ebb12fa0aab4851829b8f1768f916dd
Josje <paul.verbael@gmail.com>
2025-08-15 22:04:01 +0100

    Update WsopCode enum values and naming
    Changed CLOSE, PING, and PONG codes to match standard WebSocket opcodes. Renamed MIME_* constants to more concise names (e.g., TEXT, HTML, CSS) and removed 'MIME_' prefix for clarity.

--- src/main/java/nl/radiantrealm/library/http/WsopCode.java
+++ src/main/java/nl/radiantrealm/library/http/WsopCode.java
@@ -2,32 +2,32 @@ package nl.radiantrealm.library.http;
 
 public enum WsopCode {
     CONTINUATION(0x00),
-    CLOSE(0x01),
-    PING(0x02),
-    PONG(0x03),
+    CLOSE(0x08),
+    PING(0x09),
+    PONG(0x0A),
 
-    MIME_TEXT(0x80, MimeType.TEXT),
-    MIME_HTML(0x81, MimeType.HTML),
-    MIME_CSS(0x82, MimeType.CSS),
-    MIME_JS(0x83, MimeType.JS),
+    TEXT(0x80, MimeType.TEXT),
+    HTML(0x81, MimeType.HTML),
+    CSS(0x82, MimeType.CSS),
+    JS(0x83, MimeType.JS),
 
-    MIME_JSON(0x84, MimeType.JSON),
-    MIME_XML(0x85, MimeType.XML),
-    MIME_PDF(0x86, MimeType.PDF),
-    MIME_ZIP(0x87, MimeType.ZIP),
-    MIME_BINARY(0x88, MimeType.BINARY),
+    JSON(0x84, MimeType.JSON),
+    XML(0x85, MimeType.XML),
+    PDF(0x86, MimeType.PDF),
+    ZIP(0x87, MimeType.ZIP),
+    BINARY(0x88, MimeType.BINARY),
 
-    MIME_JPEG(0x89, MimeType.JPEG),
-    MIME_PNG(0x8A, MimeType.PNG),
-    MIME_GIF(0x8B, MimeType.GIF),
-    MIME_SVG(0x8C, MimeType.SVG),
+    JPEG(0x89, MimeType.JPEG),
+    PNG(0x8A, MimeType.PNG),
+    GIF(0x8B, MimeType.GIF),
+    SVG(0x8C, MimeType.SVG),
 
-    MIME_MP3(0x8D, MimeType.MP3),
-    MIME_OGG(0x8E, MimeType.OGG),
-    MIME_WAV(0x8F, MimeType.WAV),
+    MP3(0x8D, MimeType.MP3),
+    OGG(0x8E, MimeType.OGG),
+    WAV(0x8F, MimeType.WAV),
 
-    MIME_MP4(0x90, MimeType.MP4),
-    MIME_WEBM(0x91, MimeType.WEBM);
+    MP4(0x90, MimeType.MP4),
+    WEBM(0x91, MimeType.WEBM);
 
     public final int code;
     public final MimeType mimeType;
