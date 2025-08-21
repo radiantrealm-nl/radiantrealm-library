b75cda9570f8f985c384f3c2a3b09cb4eb663d1f
Josje <paul.verbael@gmail.com>
2025-08-22 00:38:20 +0200

    Moved handshake logic to WebsocketHandler

--- src/main/java/nl/radiantrealm/library/http/websocket/WebsocketHandler.java
+++ src/main/java/nl/radiantrealm/library/http/websocket/WebsocketHandler.java
@@ -1,113 +1,49 @@
 package nl.radiantrealm.library.http.websocket;
 
-import com.sun.net.httpserver.HttpExchange;
-import nl.radiantrealm.library.ApplicationService;
-import nl.radiantrealm.library.http.WsopCode;
-import nl.radiantrealm.library.utils.ByteUtils;
+import com.sun.net.httpserver.Headers;
+import nl.radiantrealm.library.http.HttpMethod;
+import nl.radiantrealm.library.http.HttpRequest;
+import nl.radiantrealm.library.http.StatusCode;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
 
-public abstract class WebsocketHandler implements ApplicationService {
-    private final InputStream inputStream;
-    private final OutputStream outputStream;
-    private final Thread thread;
-    private boolean isOpen;
+public interface WebsocketHandler {
+    void onFrame(WebsocketFrame websocketFrame);
 
-    public WebsocketHandler(HttpExchange exchange) {
-        WebsocketSession session = new WebsocketSession(exchange);
-        this.inputStream = session.inputStream();
-        this.outputStream = session.outputStream();
-        this.isOpen = false;
-
-        thread = new Thread(() -> {
-            try {
-                while (isOpen) {
-                    if (inputStream.available() < 1) {
-                        Thread.sleep(10);
-                        continue;
-                    }
-
-                    List<WebsocketFrame> websocketFrameList = new ArrayList<>();
-
-                    for (int i = 0; i < 2; i++) {
-                        WebsocketFrame websocketFrame = readFrame();
-
-                        if (!websocketFrame.finalMessage()) {
-                            i--;
-                        }
-
-                        websocketFrameList.add(websocketFrame);
-                    }
-
-                    WebsocketFrame websocketFrame = WebsocketFrame.merge(websocketFrameList.toArray(new WebsocketFrame[0]));
-                    onFrame(websocketFrame);
-                }
-            } catch (Exception e) {
-                //Grote oei, log of onFrame met lege socket?
-            }
-        });
-    }
-
-    protected abstract void onFrame(WebsocketFrame websocketFrame);
-
-    @Override
-    public void start() throws Exception {
-        this.isOpen = true;
-        thread.start();
-    }
-
-    @Override
-    public void stop() throws Exception {
-        this.isOpen = false;
-    }
-
-    protected WebsocketFrame readFrame() throws Exception {
-        byte[] headerBytes = inputStream.readNBytes(2);
-
-        boolean finalMessage = ByteUtils.getBit(headerBytes[0], 0b00000001);
-        int pli = ByteUtils.readBits(headerBytes[0], 0b00000110);
-        WsopCode wsopCode = WsopCode.valueOf(headerBytes[1]);
-
-        if (wsopCode == null) {
-            throw new IllegalArgumentException("Invalid Wsop Code.");
+    default void handshake(HttpRequest request) throws Exception {
+        if (!request.verifyRequestMethod(HttpMethod.GET)) {
+            request.sendStatusResponse(StatusCode.INVALID_METHOD);
+            return;
         }
 
-        byte[] payloadLength = switch (pli) {
-            case 0b00000010 -> inputStream.readNBytes(1);
-            case 0b00000100 -> inputStream.readNBytes(2);
-            case 0b00000110 -> inputStream.readNBytes(3);
-            default -> new byte[0];
-        };
+        Headers clientHeaders = request.getRequestHeaders();
 
-        byte[] payload = inputStream.readNBytes((int) ByteUtils.combineBytesLong(payloadLength));
+        if (!clientHeaders.getFirst("Upgrade").equalsIgnoreCase("websocket")) {
+            request.sendStatusResponse(StatusCode.BAD_REQUEST);
+            return;
+        } else if (!clientHeaders.getFirst("Upgrade").equalsIgnoreCase("connection")) {
+            request.sendStatusResponse(StatusCode.BAD_REQUEST);
+            return;
+        }
 
-        return new WebsocketFrame(System.currentTimeMillis(), finalMessage, wsopCode, payload);
-    }
+        String clientKey = clientHeaders.getFirst("Sec-WebSocket-Key");
 
-    protected void sendFrame(WebsocketFrame websocketFrame) throws Exception {
-        int pli = definePayloadLengthIndex(websocketFrame.payloadLength());
+        if (clientKey == null) {
+            request.sendStatusResponse(StatusCode.BAD_REQUEST);
+            return;
+        }
 
-        byte[] headerBytes = new byte[] {
-                (byte) ((websocketFrame.finalMessage() ? 1 : 0) + pli),
-                (byte) websocketFrame.wsopCode().code
-        };
+        String websocketKey = Base64.getEncoder().encodeToString(
+                MessageDigest.getInstance("SHA-1").digest(
+                        (clientKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes(StandardCharsets.UTF_8)
+                )
+        );
 
-        byte[] payloadLength = ByteUtils.toBytes(websocketFrame.payloadLength());
-
-        outputStream.write(headerBytes);
-        outputStream.write(payloadLength);
-        outputStream.write(websocketFrame.payload());
-        outputStream.flush();
-    }
-
-    private int definePayloadLengthIndex(int payloadLength) {
-        if (payloadLength == 0) return 0;
-        if (payloadLength <= 256) return 2;
-        if (payloadLength <= 65536) return 4;
-        if (payloadLength <= 16777216) return 6;
-        return 0;
+        request.setResponseHeaders("Upgrade", "websocket");
+        request.setResponseHeaders("Connection", "Upgrade");
+        request.setResponseHeaders("Sec-Websocket-Accept", websocketKey);
+        request.sendStatusResponse(StatusCode.SWITCHING_PROTOCOLS);
     }
 }
