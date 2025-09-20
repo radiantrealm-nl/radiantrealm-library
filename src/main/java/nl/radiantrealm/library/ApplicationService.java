d749cdf31bf6d6561cb217ae53208c2a7bbce9b8
Josje <paul.verbael@gmail.com>
2025-09-21 01:25:18 +0200

    Enhance ApplicationService with status and command methods
    Added a status() method returning a JsonObject with running_since, a command(JsonObject) method throwing by default, and a serviceType() method. Also updated start() to set runningSince and improved method signatures to throw Exception.

--- src/main/java/nl/radiantrealm/library/ApplicationService.java
+++ src/main/java/nl/radiantrealm/library/ApplicationService.java
@@ -1,19 +1,34 @@
 package nl.radiantrealm.library;
 
+import com.google.gson.JsonObject;
+
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 public interface ApplicationService {
     AtomicBoolean isRunning = new AtomicBoolean(false);
+    AtomicLong runningSince = new AtomicLong(System.currentTimeMillis());
 
-    default void start() {
-        isRunning.set(true);
+    default String serviceType() {
+        return "Unknown service type.";
     }
 
-    default void stop() {
+    default void start() throws Exception {
+        isRunning.set(true);
+        runningSince.set(System.currentTimeMillis());
+    }
+
+    default void stop() throws Exception {
         isRunning.set(false);
     }
 
-    default boolean isRunning() {
-        return isRunning.get();
+    default JsonObject status() throws Exception {
+        JsonObject object = new JsonObject();
+        object.addProperty("running_since", runningSince.get());
+        return object;
+    }
+
+    default void command(JsonObject object) throws Exception {
+        throw new IllegalArgumentException("Not implemented.");
     }
 }
