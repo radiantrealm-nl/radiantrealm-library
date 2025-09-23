b65c16b1e41872823f6a5398e868292e54c7f337
Josje <paul.verbael@gmail.com>
2025-09-23 21:10:05 +0200

    Add handle and callback methods to Process record
    Introduces handle() to delegate processing to the handler and callback() to invoke the consumer with the result, enhancing the Process record's functionality.

--- src/main/java/nl/radiantrealm/library/processor/Process.java
+++ src/main/java/nl/radiantrealm/library/processor/Process.java
@@ -4,4 +4,15 @@ import com.google.gson.JsonObject;
 
 import java.util.function.Consumer;
 
-public record Process(int processID, ProcessHandler handler, JsonObject object, Consumer<ProcessResult> consumer) {}
+public record Process(int processID, ProcessHandler handler, JsonObject object, Consumer<ProcessResult> consumer) {
+
+    public ProcessResult handle() throws Exception {
+        return handler.handle(this);
+    }
+
+    public void callback(ProcessResult result) {
+        if (consumer != null) {
+            consumer.accept(result);
+        }
+    }
+}
