e17cdcfe0e378570f9fe2de4654970ea92997da0
Josje <101420019+ome-jos@users.noreply.github.com>
2025-09-24 10:55:48 +0200

    Add tryFunction methods for error handling

--- src/main/java/nl/radiantrealm/library/processor/ProcessException.java
+++ src/main/java/nl/radiantrealm/library/processor/ProcessException.java
@@ -6,4 +6,20 @@ public class ProcessException extends Exception {
     public ProcessException(ProcessResult result) {
         this.result = result;
     }
+
+    public static <T, R> R tryFunction(ThrowingFunction<T, R> function, T arg1, HttpResponse response) throws Exception {
+        try {
+            return function.apply(arg1);
+        } catch (IllegalArgumentException e) {
+            throw new HttpException(response);
+        }
+    }
+
+    public static <T, U, R> R tryFunction(ThrowingBiFunction<T, U, R> function, T arg1, U arg2, HttpResponse response) throws Exception {
+        try {
+            return function.apply(arg1, arg2);
+        } catch (IllegalArgumentException e) {
+            throw new HttpException(response);
+        }
+    }
 }
