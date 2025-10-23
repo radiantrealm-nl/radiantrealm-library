bcc6aa44126b0ae293b7f873524abc6deb548c96
Josje <paul.verbael@gmail.com>
2025-10-23 22:23:35 +0200

    Add HttpResponse record for HTTP responses
    Introduces the HttpResponse record to encapsulate HTTP response data, including status code, media type, and response body. Provides multiple constructors for convenient creation from StatusCode and JsonObject instances.

---
+++ src/main/java/nl/radiantrealm/library/http/model/HttpResponse.java
@@ -0,0 +1,37 @@
+package nl.radiantrealm.library.http.model;
+
+import nl.radiantrealm.library.http.enumerator.MediaType;
+import nl.radiantrealm.library.http.enumerator.StatusCode;
+import nl.radiantrealm.library.utils.json.JsonObject;
+
+public record HttpResponse(int statusCode, String mediaType, String responseBody) {
+
+    public HttpResponse(StatusCode statusCode, JsonObject object) {
+        this(
+                statusCode.code,
+                MediaType.JSON.type,
+                object.getAsString()
+        );
+    }
+
+    public HttpResponse(StatusCode statusCode) {
+        this(
+                statusCode,
+                statusCode.toJson()
+        );
+    }
+
+    public HttpResponse(StatusCode statusCode, String message) {
+        this(
+                statusCode,
+                statusCode.toJson(message)
+        );
+    }
+
+    public HttpResponse(StatusCode statusCode, String key, String message) {
+        this(
+                statusCode,
+                statusCode.toJson(key, message)
+        );
+    }
+}
