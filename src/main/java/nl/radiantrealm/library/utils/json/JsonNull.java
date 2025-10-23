6380f1c83b6d434bd8cddddbd662009e42a4ae34
Josje <paul.verbael@gmail.com>
2025-10-23 22:13:11 +0200

    Imported radiantrealm-json-library
    Actual peak Json parsing with modern Java 21

---
+++ src/main/java/nl/radiantrealm/library/utils/json/JsonNull.java
@@ -0,0 +1,15 @@
+package nl.radiantrealm.library.utils.json;
+
+public class JsonNull extends JsonElement {
+    public static final JsonNull INSTANCE = new JsonNull();
+
+    @Override
+    public JsonElement deepCopy() {
+        return INSTANCE;
+    }
+
+    @Override
+    protected String toString(StringBuilder builder, boolean prettyPrint) {
+        return "null";
+    }
+}
