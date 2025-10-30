69ba98106d1ead3d662f1f5db61eb5ebb0e2b4ed
Josje <paul.verbael@gmail.com>
2025-10-30 15:03:45 +0100

    Update CachingStrategy.java
    Updated eviction duration and intervals to long, while keeping a method to instantiate them with duration.

--- src/main/java/nl/radiantrealm/library/cache/CachingStrategy.java
+++ src/main/java/nl/radiantrealm/library/cache/CachingStrategy.java
@@ -1,9 +1,20 @@
 package nl.radiantrealm.library.cache;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.Duration;
+import java.util.Objects;
 
 public record CachingStrategy(
-        Duration evictionDuration,
-        Duration evictionInterval,
+        long evictionDuration,
+        long evictionInterval,
         int maxCacheEntries
-) {}
+) {
+    public CachingStrategy(@NotNull Duration evictionDuration, @NotNull Duration evictionInterval, int maxCacheEntries) {
+        this(
+                Objects.requireNonNull(evictionDuration).toMillis(),
+                Objects.requireNonNull(evictionInterval).toMillis(),
+                maxCacheEntries
+        );
+    }
+}
