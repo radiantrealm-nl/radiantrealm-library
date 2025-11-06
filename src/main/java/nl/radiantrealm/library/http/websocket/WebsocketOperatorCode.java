31fce526a91de90032e23ff34e01043e11285e9b
Josje <paul.verbael@gmail.com>
2025-11-06 23:41:33 +0100

    Fix typo in WebsocketOperatorCode enum value
    Corrected 'BIINARY' to 'BINARY' in the WebsocketOperatorCode enum to fix a spelling error.

--- src/main/java/nl/radiantrealm/library/http/websocket/WebsocketOperatorCode.java
+++ src/main/java/nl/radiantrealm/library/http/websocket/WebsocketOperatorCode.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public enum WebsocketOperatorCode {
     CONTINUE(0x00),
     UTF_8(0x01),
-    BIINARY(0x02),
+    BINARY(0x02),
     CLOSE(0x08),
     PING(0x09),
     PONG(0x0A);
