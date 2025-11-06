05b2ebaeec7f04a08760838932cac173a0f42b4d
Josje <paul.verbael@gmail.com>
2025-11-06 23:41:06 +0100

    Update WebsocketConfiguration.java

--- src/main/java/nl/radiantrealm/library/http/websocket/WebsocketConfiguration.java
+++ src/main/java/nl/radiantrealm/library/http/websocket/WebsocketConfiguration.java
@@ -3,9 +3,34 @@ package nl.radiantrealm.library.http.websocket;
 import java.net.InetSocketAddress;
 
 public record WebsocketConfiguration(
-        InetSocketAddress address,
+        // 1. Address
+        InetSocketAddress socketAddress,
+
+        // 2. Peformance and sizing
+        int workingIOThreads,
+        int maxActiveSessions,
         int incomingBufferSize,
         int sessionBufferSize,
-        int maxPayloadLength
+        int maxFrameLength,
+        int maxPayloadLength,
+
+        // 3. Protocol and security
+        boolean enforceBitMasking,
+        boolean allowFragmentation,
+        int maxPingIntervalSeconds,
+        int sessionTimeoutSeconds
 ) {
+    public static final WebsocketConfiguration defaultConfiguration = new WebsocketConfiguration(
+            new InetSocketAddress(0),
+            10,
+            1024,
+            1024,
+            1024,
+            Integer.MAX_VALUE,
+            Integer.MAX_VALUE,
+            true,
+            true,
+            30,
+            300
+    );
 }
