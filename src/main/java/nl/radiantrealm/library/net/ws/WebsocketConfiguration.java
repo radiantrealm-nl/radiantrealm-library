62dcb3ccfa26a7703d29a7509d2e92ec1c43c121
Josje <paul.verbael@gmail.com>
2025-12-01 00:19:34 +0100

    Update WebsocketConfiguration fields and defaults
    Renamed 'maxConcurrentlimit' to 'maxActiveSessions' and added 'incomingBufferSize' and 'outgoingBufferSize' fields. Updated default configuration values to reflect these changes.

--- src/main/java/nl/radiantrealm/library/net/ws/WebsocketConfiguration.java
+++ src/main/java/nl/radiantrealm/library/net/ws/WebsocketConfiguration.java
@@ -2,14 +2,18 @@ package nl.radiantrealm.library.net.ws;
 
 public record WebsocketConfiguration(
         int threadPoolSize,
-        int maxConcurrentlimit,
+        int maxActiveSessions,
+        int incomingBufferSize,
+        int outgoingBufferSize,
         int sessionTimeoutMillis,
         int pingTimeoutMillis,
         int closeTimeoutMillis
 ) {
     public static final WebsocketConfiguration defaultConfiguration = new WebsocketConfiguration(
-            20,
-            1000,
+            16,
+            1024,
+            1024,
+            1024,
             300000,
             5000,
             5000
