fcb585a844add0a5ffa0369dce437695ecc376ab
Josje <paul.verbael@gmail.com>
2025-12-14 23:43:40 +0100

    Refactor WebsocketConfiguration and add validation
    Removed the maxActiveSessions parameter and implemented the SocketConfiguration interface. Added a custom constructor to validate and enforce minimum values for configuration parameters. Updated the defaultConfiguration accordingly.

--- src/main/java/nl/radiantrealm/library/net/ws/WebsocketConfiguration.java
+++ src/main/java/nl/radiantrealm/library/net/ws/WebsocketConfiguration.java
@@ -1,18 +1,36 @@
 package nl.radiantrealm.library.net.ws;
 
+import nl.radiantrealm.library.net.io.SocketConfiguration;
+
 public record WebsocketConfiguration(
         int threadPoolSize,
-        int maxActiveSessions,
         int incomingBufferSize,
         int outgoingBufferSize,
         int checkTimeoutMillis,
         int sessionTimeoutMillis,
         int pingTimeoutMillis,
         int closeTimeoutMillis
-) {
+) implements SocketConfiguration {
+    public WebsocketConfiguration(
+            int threadPoolSize,
+            int incomingBufferSize,
+            int outgoingBufferSize,
+            int checkTimeoutMillis,
+            int sessionTimeoutMillis,
+            int pingTimeoutMillis,
+            int closeTimeoutMillis
+    ) {
+        this.threadPoolSize = Math.max(1, threadPoolSize);
+        this.incomingBufferSize = Math.max(1, incomingBufferSize);
+        this.outgoingBufferSize = Math.max(1, outgoingBufferSize);
+        this.checkTimeoutMillis = Math.max(1, checkTimeoutMillis);
+        this.sessionTimeoutMillis = Math.max(1, sessionTimeoutMillis);
+        this.pingTimeoutMillis = Math.max(1, pingTimeoutMillis);
+        this.closeTimeoutMillis = Math.max(1, closeTimeoutMillis);
+    }
+
     public static final WebsocketConfiguration defaultConfiguration = new WebsocketConfiguration(
             16,
-            1024,
             1024,
             1024,
             5000,
