2e971da5de194f0760684604fe7ffe61a6fbe97e
Josje <paul.verbael@gmail.com>
2025-12-14 23:44:31 +0100

    Add WebsocketServer class implementing HttpHandler
    Introduces the abstract WebsocketServer class, extending WebsocketEngine and implementing HttpHandler. This class handles HTTP connections by performing the WebSocket handshake, managing connection upgrades, and integrating with the selector for asynchronous processing.

---
+++ src/main/java/nl/radiantrealm/library/net/ws/WebsocketServer.java
@@ -0,0 +1,25 @@
+package nl.radiantrealm.library.net.ws;
+
+import nl.radiantrealm.library.net.http.HttpConnection;
+import nl.radiantrealm.library.net.http.HttpHandler;
+import nl.radiantrealm.library.net.http.HttpRequest;
+import nl.radiantrealm.library.net.http.HttpResponse;
+
+import java.io.IOException;
+
+public abstract class WebsocketServer extends WebsocketEngine implements HttpHandler {
+
+    public WebsocketServer(WebsocketConfiguration configuration) throws IOException {
+        super(configuration);
+    }
+
+    @Override
+    public void handle(HttpConnection connection, HttpRequest request) {
+        HttpResponse response = WebsocketHandshake.performHandshake(request);
+
+        connection.key.cancel();
+        connection.addOutboundBuffer(response.getBytes());
+        pendingUpgrades.add(connection);
+        selector.wakeup();
+    }
+}
