31b665194e62abcde905751d2ea193beb0d5e3ac
Josje <paul.verbael@gmail.com>
2025-12-14 23:41:16 +0100

    Add HTTP/3 support to HttpVersion enum
    Introduced HTTP_3 to the HttpVersion enum and updated the getVersion method to recognize the "HTTP/3" string. This allows the library to handle HTTP/3 protocol versions.

--- src/main/java/nl/radiantrealm/library/net/http/HttpVersion.java
+++ src/main/java/nl/radiantrealm/library/net/http/HttpVersion.java
@@ -3,13 +3,15 @@ package nl.radiantrealm.library.net.http;
 public enum HttpVersion {
     HTTP_1_0("HTTP/1.0"),
     HTTP_1_1("HTTP/1.1"),
-    HTTP_2("HTTP/2");
+    HTTP_2("HTTP/2"),
+    HTTP_3("HTTP/3");
 
     public static HttpVersion getVersion(String string) {
-        return switch (string.toUpperCase()) {
+        return switch (string) {
             case "HTTP/1.0" -> HTTP_1_0;
             case "HTTP/1.1" -> HTTP_1_1;
             case "HTTP/2" -> HTTP_2;
+            case "HTTP/3" -> HTTP_3;
             default -> null;
         };
     }
